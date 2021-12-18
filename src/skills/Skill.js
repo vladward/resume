3309fc77158273cb59e2c1a19efbb73f9e18c17d
vladward <vladward13@gmail.com>
2021-12-18 15:49:37 +0300

    feat: fill Skill code

---
+++ src/skills/Skill.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Skill.module.css'
+
+export function Skill(props) {
+    return (
+        <div className={s.skill}>
+            <div className={s.icon}>
+
+            </div>
+            <h3>{props.title}</h3>
+            <span className={s.description}>{props.description}</span>
+        </div>
+    )
+}

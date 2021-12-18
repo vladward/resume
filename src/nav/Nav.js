2b009bff72f835683d1f9aefd6a1bb5a2514085b
vladward <vladward13@gmail.com>
2021-12-18 15:49:44 +0300

    feat: fill Nav code

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -3,8 +3,11 @@ import s from './Nav.module.css'
 
 export function Nav() {
     return (
-        <div className={s.header}>
-
+        <div className={s.nav}>
+            <a href="">Main</a>
+            <a href="">Works</a>
+            <a href="">Skills</a>
+            <a href="">Contacts</a>
         </div>
-    );
+    )
 }

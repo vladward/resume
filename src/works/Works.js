577994f3aa7607680bad69f1b8be185116aa693d
vladward <vladward13@gmail.com>
2021-12-24 15:03:41 +0300

    feat: create Works component

---
+++ src/works/Works.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import s from './Works.module.css'
+import c from './../common/styles/Container.module.css'
+
+export const Works = () => {
+    return (
+        <div className={s.works}>
+            <div className={c.container}>
+                <h2>Works</h2>
+                <div className={s.workItem}>
+
+                </div>
+            </div>
+        </div>
+    )
+}

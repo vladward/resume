d92cc8eea99a1f8fae225ec940369ffddc77895c
vladward <vladward13@gmail.com>
2022-01-30 21:04:06 +0300

    feat: add work component in works

--- src/works/Works.js
+++ src/works/Works.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import s from './Works.module.css'
+import s from './Works.module.scss'
 import c from './../common/styles/Container.module.css'
+import {Work} from "./Work";
+import {Title} from "../common/Title/Title";
 
 export const Works = () => {
     return (
         <div className={s.works}>
-            <div className={c.container}>
-                <h2>Works</h2>
-                <div className={s.workItem}>
+            <div className={`${c.container} ${s.worksContainer}`}>
+                <Title title={'My works'}/>
+                <div className={s.worksItem}>
 
+                    <Work
+                        img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXBBU2BuAf62cEvN35u62QVIeWeu2nN6hpjQ&usqp=CAU'
+                        title='google project'
+                        description='not bad google project'
+                    />
+                    <Work
+                        img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXBBU2BuAf62cEvN35u62QVIeWeu2nN6hpjQ&usqp=CAU'
+                        title='google project'
+                        description='not bad google project'/>
+                    <Work
+                        img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXBBU2BuAf62cEvN35u62QVIeWeu2nN6hpjQ&usqp=CAU'
+                        title='google project'
+                        description='not bad google project'
+                    />
+                    <Work
+                        img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXBBU2BuAf62cEvN35u62QVIeWeu2nN6hpjQ&usqp=CAU'
+                        title='google project'
+                        description='not bad google project'/>
                 </div>
             </div>
         </div>
